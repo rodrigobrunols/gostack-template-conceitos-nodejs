c8ed01b4e20918280bfe702207bf72b639bc92fa
leoncio <rodrigo.leoncio@sicoob.com.br>
2020-10-31 18:31:37 -0300

    primeiro commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
+const{uuid} = require('uuidv4');
+
 
 // const { v4: uuid, validate: isUuid } = require('uuid');
 
@@ -11,23 +13,74 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+   return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title,url,techs} = request.body;
+  
+  const repository = {
+    id:uuid(), 
+    title, 
+    url, 
+    techs,
+    likes: 0
+  };
+  
+  repositories.push(repository)
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const {title, url, techs,likes} = request.body;
+  const index = repositories.findIndex(repo => repo.id === id);
+
+  if(index < 0) {
+      return response.status(404).json({error : "repository not found"});
+  }
+
+  const newRepository = {
+      id,
+      url,
+      title,
+      techs,
+      likes
+  }
+  repositories[index] = newRepository;
+
+  return response.json(newRepository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", (req, res) => {
+  const {id} = req.params;
+
+  const repoIndex = repositories.findIndex(repository => repository.id == id);
+
+  if(repoIndex < 0) {
+      return res.status(404).json({error : "repository not found"});
+  }
+  repositories.splice(repoIndex, 1);
+
+  return res.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", (req, res) => {
+  const {id} = req.params;
+
+  const repoIndex = repositories.findIndex(repository => repository.id == id);
+
+  if(repoIndex < 0) {
+      return res.status(404).json({error : "repository not found"});
+  }
+
+  const repo = repositories[repoIndex];
+  repo.likes++;
+
+  return res.status(201).json(repo);
+
 });
 
 module.exports = app;
